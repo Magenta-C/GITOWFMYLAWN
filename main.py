e715e328e21290811814b8f95973005288b26639
xXZoomerXx <can0008@brauer.vic.edu.au>
2020-08-10 11:48:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random_poem
+
+print("I'm QWERT the freindly chatbot")
+print("What is your name?")
+name = input()
+print(name, "is a nice name")
+print("do you want a poem? (yes or no)")
+poem = input()
+if peom
